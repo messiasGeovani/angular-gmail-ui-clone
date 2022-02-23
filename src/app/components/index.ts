4ea914af01cac64e8d157958eb1ee526d811e0ab
messiasGeovani <messias.geovani.lima@gmail.com>
2022-02-23 17:51:38 -0300

    search input component config

---
+++ src/app/components/index.ts
@@ -0,0 +1,2 @@
+export * from './molecules/search-input/search-input.component';
+export * from './organisms/topbar/topbar.component';
---
+++ src/app/components/molecules/search-input/search-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.scss'],
+})
+export class SearchInputComponent implements OnInit {
+  topbarFormControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  value = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
